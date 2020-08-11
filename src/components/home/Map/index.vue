93dddc7bba43f0545f14a7886b98e480defb67ff
e <zcs02469@163.com>
2020-08-11 23:01:16 +0800

    删除无用图片

--- src/components/home/Map/index.vue
+++ src/components/home/Map/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="panel">
+        <div class="map-box"></div>
         <div class="footer-panel">今日航班数<span class="max">&nbsp;&nbsp;715&nbsp;&nbsp;</span>单</div>
     </div>
 </template>
@@ -13,6 +14,12 @@ export default {};
     width: 100%;
     height: 678.5px;
     position: relative;
+}
+.map-box {
+    width: 100%;
+    height: 610px;
+    background: url('../../../assets/image/timg3.png');
+    background-size: 100% 100%;
 }
 .footer-panel {
     height: 40px;
