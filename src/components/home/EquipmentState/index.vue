27b7aadff79a40644f6cea740cff6457b2e7b92e
e <zcs02469@163.com>
2020-10-20 00:36:54 +0800

    修改设备异常模块数据

--- src/components/home/EquipmentState/index.vue
+++ src/components/home/EquipmentState/index.vue
@@ -14,10 +14,17 @@
         </div>
         <div class="content">
             <ul>
-                <li v-for="(item,index) in expDeviceInfoList" :key="`system-device${index}`">
+                <!-- <li v-for="(item,index) in expDeviceInfoList" :key="`system-device${index}`">
                     <div class="left">{{item.expQuantity}}（{{item.totalQuantity}}）</div>
                     <div class="right">
-                        <div class="name">正常设备{{item.normalQuantity}}</div>
+                        <div class="name">正常{{item.normalQuantity}}</div>
+                        <div class="type">异常{{item.expQuantity}}</div>
+                    </div>
+                </li> -->
+                <li v-for="(item,index) in list" :key="`system-device${index}`">
+                    <div class="left">{{item.systemName}}</div>
+                    <div class="right">
+                        <div class="name">正常{{item.normalQuantity}}</div>
                         <div class="type">异常{{item.expQuantity}}</div>
                     </div>
                 </li>
@@ -33,28 +40,7 @@ export default {
     data() {
         return{
             todayDeviceException:"",
-            expDeviceInfoList:[
-                {
-                    totalQuantity:1,    //全部设备数量
-                    expQuantity:0,      //异常设备数量
-                    normalQuantity: 1   //正常设备数量
-                },
-                {
-                    totalQuantity:1,
-                    expQuantity:0,
-                    normalQuantity: 1
-                },
-                {
-                    totalQuantity:1,
-                    expQuantity:0,
-                    normalQuantity: 1
-                },
-                {
-                    totalQuantity:1,
-                    expQuantity:0,
-                    normalQuantity: 1
-                },
-            ],
+            list:[],
             isShow: true
         }
     },
@@ -68,6 +54,7 @@ export default {
         },
         async getData() {
             let [res] = await getDeviceStatus();
+            // let res = {"result":"true","message":"{\"todayDeviceException\":\"中心4#冷水机组停机维修\",\"expDeviceInfoList\":[{\"pageNo\":null,\"orderBy\":null,\"isNewRecord\":false,\"pageSize\":null,\"id\":\"1296355281540980736\",\"status\":null,\"remarks\":null,\"createByName\":null,\"createDate\":null,\"updateDate\":null,\"lastUpdateDateTime\":null,\"updateBy\":null,\"createBy\":null,\"updateByName\":null,\"expId\":\"1296355281540980736\",\"systemName\":\"弱电系统\",\"expQuantity\":1,\"normalQuantity\":7,\"createDate_lte\":null,\"updateDate_between\":null,\"createDate_between\":null,\"createDate_gte\":null,\"updateDate_gte\":null,\"updateDate_lte\":null,\"status_in\":null,\"id_in\":null},{\"pageNo\":null,\"orderBy\":null,\"isNewRecord\":false,\"pageSize\":null,\"id\":\"1296355231188361216\",\"status\":null,\"remarks\":null,\"createByName\":null,\"createDate\":null,\"updateDate\":null,\"lastUpdateDateTime\":null,\"updateBy\":null,\"createBy\":null,\"updateByName\":null,\"expId\":\"1296355231188361216\",\"systemName\":\"暖通系统\",\"expQuantity\":0,\"normalQuantity\":2,\"createDate_lte\":null,\"updateDate_between\":null,\"createDate_between\":null,\"createDate_gte\":null,\"updateDate_gte\":null,\"updateDate_lte\":null,\"status_in\":null,\"id_in\":null},{\"pageNo\":null,\"orderBy\":null,\"isNewRecord\":false,\"pageSize\":null,\"id\":\"1296354954062307328\",\"status\":null,\"remarks\":null,\"createByName\":null,\"createDate\":null,\"updateDate\":null,\"lastUpdateDateTime\":null,\"updateBy\":null,\"createBy\":null,\"updateByName\":null,\"expId\":\"1296354954062307328\",\"systemName\":\"给排水系统\",\"expQuantity\":2,\"normalQuantity\":2,\"createDate_lte\":null,\"updateDate_between\":null,\"createDate_between\":null,\"createDate_gte\":null,\"updateDate_gte\":null,\"updateDate_lte\":null,\"status_in\":null,\"id_in\":null},{\"pageNo\":null,\"orderBy\":null,\"isNewRecord\":false,\"pageSize\":null,\"id\":\"1296354386371649536\",\"status\":null,\"remarks\":null,\"createByName\":null,\"createDate\":null,\"updateDate\":null,\"lastUpdateDateTime\":null,\"updateBy\":null,\"createBy\":null,\"updateByName\":null,\"expId\":\"1296354386371649536\",\"systemName\":\"供配电系统\",\"expQuantity\":2,\"normalQuantity\":4,\"createDate_lte\":null,\"updateDate_between\":null,\"createDate_between\":null,\"createDate_gte\":null,\"updateDate_gte\":null,\"updateDate_lte\":null,\"status_in\":null,\"id_in\":null}]}"}
             let {todayDeviceException,expDeviceInfoList} = JSON.parse(res.message);
             // expDeviceInfoList	设备状态对象
             // expQuantity	异常设备数量
@@ -75,11 +62,22 @@ export default {
 
             this.todayDeviceException = todayDeviceException;
             // this.todayDeviceException = "asdfgnakdfjlkajflkajfdl;asdfasd发生发发打发;lasjdflkajsdklf";
+            let arr = [];
             expDeviceInfoList.forEach((item,i) => {
-                this.expDeviceInfoList[i].totalQuantity = item.totalQuantity;
-                this.expDeviceInfoList[i].expQuantity = item.expQuantity;
-                this.expDeviceInfoList[i].normalQuantity = Number(item.totalQuantity) - Number(item.expQuantity);
+                arr.push({
+                    systemName: item.systemName,
+                    normalQuantity: item.normalQuantity,
+                    expQuantity: item.expQuantity,
+                })
+                // this.expDeviceInfoList[i].systemName = item.systemName;
+                // this.expDeviceInfoList[i].normalQuantity = item.normalQuantity;//正常
+                // this.expDeviceInfoList[i].expQuantity = item.expQuantity;//异常
+                // this.expDeviceInfoList[i].totalQuantity = item.totalQuantity;
+                // this.expDeviceInfoList[i].expQuantity = item.expQuantity;
+                // this.expDeviceInfoList[i].normalQuantity = Number(item.totalQuantity) - Number(item.expQuantity);
             });
+            this.list = arr;
+            this.list.splice();
             this.$nextTick(()=> {
                 this.textScroll();
             })
@@ -123,7 +121,7 @@ export default {
                 box.scrollLeft = 0;
                 toScrollLeft();
             }
-        },
+        }
     }
 };
 </script>
