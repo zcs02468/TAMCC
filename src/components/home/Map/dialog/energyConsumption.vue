3ddb9814c39ac5d4049161215648584f0db6be45
e <zcs02469@163.com>
2021-01-08 00:02:09 +0800

    444

--- src/components/home/Map/dialog/energyConsumption.vue
+++ src/components/home/Map/dialog/energyConsumption.vue
@@ -47,6 +47,8 @@ export default {
             factArr:[],
             directionStyle:``,
             isShowDirection: false,
+            quotaObj:{},
+            actualYData:{},
             options:[]
         }
     },
